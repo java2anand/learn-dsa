ce0f1a47153bf3b6a61a72716f09646dd5ad8ccd
techjanand <abhishek.anand@techjockey.com>
2023-04-29 23:58:59 +0530

    week 1 assignment

---
+++ 2.week1_assignments_pattern/2.hollow_square_pattern.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter a number";
+    cin >> n;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            //std::cout << (i == 0 || i == n - 1 || j == 0 || j == n - 1 ? "* " : "  ");
+
+            if(i == 0 || i== n-1 || j==0 || j==n-1){
+                cout << "* ";
+            }else{
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+}
---
+++ 2.week1_assignments_pattern/4.full_pyramid.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter a number";
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        int k = 0;
+        for(int j=0; j< (n*2 - 1); j++){
+            if(j < n-i-1){
+                cout << "  ";
+            }else if(k < i*2+1){
+                cout << "* ";
+                k++;
+            }else{
+                cout << "  ";
+            }
+            
+        }
+        cout << endl;
+    }
+}
